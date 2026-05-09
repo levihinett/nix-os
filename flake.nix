a14710fc6e9298d7555ef21acd4aa9a91c854a1c
levihinett <levihinett@gmail.com>
2026-05-09 21:21:15 +0100

    Initial flakebased hyprland

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Levi NixOS config";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }:
+  let
+    system = "x86_64-linux";
+  in {
+    nixosConfigurations.hp-envy =
+      nixpkgs.lib.nixosSystem {
+        inherit system;
+
+        modules = [
+          ./hosts/hp-envy/configuration.nix
+
+          home-manager.nixosModules.home-manager
+
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            home-manager.users.levih = import ./home/levi.nix;
+          }
+        ];
+      };
+  };
+}
---
+++ home/levi.nix
@@ -0,0 +1,95 @@
+{ pkgs, ... }:
+
+{
+  home.username = "levih";
+  home.homeDirectory = "/home/levih";
+
+  home.stateVersion = "25.11";
+
+  programs.home-manager.enable = true;
+
+  home.packages = with pkgs; [
+    kitty
+    neovim
+    git
+    gh
+    tmux
+    zoxide
+    eza
+    bat
+    fzf
+    ripgrep
+    fd
+    starship
+    cliphist
+    fastfetch
+    btop
+    yazi
+    floorp-bin
+    
+    wl-clipboard
+    cliphist
+    vscodium
+    
+    waybar
+    rofi
+    mako
+    wl-clipboard
+    grim
+    slurp
+    brightnessctl
+    playerctl
+  ];
+
+  wayland.windowManager.hyprland = {
+      enable = true;
+      systemd.enable = false; 
+
+      settings = {
+        "$mod" = "SUPER";
+        "$terminal" = "kitty";
+
+      monitor = [
+        ",preferred,auto,1"
+      ];
+
+     exec-once = [
+        "waybar"
+        "mako"
+      ];
+
+      bind = [
+        "$mod, RETURN, exec, kitty"
+        "$mod, Q, killactive"
+        "$mod, M, exit"
+        "$mod, D, exec, rofi -show drun"
+        "$mod, E, exec, kitty -e yazi"
+        "$mod, F, fullscreen"
+      
+        "$mod, left, movefocus, l"
+        "$mod, right, movefocus, r"
+        "$mod, up, movefocus, u"
+        "$mod, down, movefocus, d"
+
+        "$mod SHIFT, left, movewindow, l"
+        "$mod SHIFT, right, movewindow, r"
+        "$mod SHIFT, up, movewindow, u"
+        "$mod SHIFT, down, movewindow, d"
+      ];
+
+      general = {
+        gaps_in = 5;
+        gaps_out = 10;
+        border_size = 2;
+      };
+
+      decoration = {
+        rounding = 8;
+      };
+
+      input = {
+        kb_layout = "gb";
+      };
+    };
+  };
+}
