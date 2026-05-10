1d9f03fbbdfd6a854cc006db5ea97ac94dffd925
levihinett <levihinett@gmail.com>
2026-05-10 19:58:13 +0100

    portably hyprland conf

--- home/modules/spicetify/default.nix
+++ home/modules/spicetify/default.nix
@@ -1,17 +1,13 @@
-{ pkgs, lib, ... , inputs }:
-
+{ pkgs, lib, inputs, ... }:
 {
   home.packages = with pkgs; [
-    # Install the Spicetify package from the flake input
     inputs.spicetify-nix.packages.${pkgs.system}.spicetify
   ];
 
-  # Import the official spicetify-nix Home Manager module
   imports = [
     inputs.spicetify-nix.homeManagerModules.default
   ];
 
-  # Declarative configuration
   programs.spicetify = {
     enable = true;
     theme = "catppuccin";
